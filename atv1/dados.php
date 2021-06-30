6d50a365e80562907e1aee0c687b9976c820fc1b
Alisson Alves <alisson.a.m88@gmail.com>
2021-06-29 21:12:13 -0300

    Add files via upload

---
+++ atv1/dados.php
@@ -0,0 +1,15 @@
+<?php
+  /* Questão 01
+  if(isset($_POST['sports'])){
+    foreach($_POST['sports'] as $esporte){
+      echo "- " . $esporte . "</br>";
+    }
+    echo "<a href='index.php'>Voltar</a>";
+  }
+  */
+
+  /* Questão 02
+  echo "Sua faixa etária é de: ". $_POST['age'] . "</br>";
+  echo "<a href='index.php'>Voltar</a>"
+  */
+?>
---
+++ atv1/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Atividade 1</title>
+</head>
+<body>
+  <!-- Questão 01
+  <form action="dados.php" method="post">
+    <div class="content">
+      <input type="checkbox" name="sports[]" value="Futebol"> Futebol
+      <input type="checkbox" name="sports[]" value="Natação"> Natação
+      <input type="checkbox" name="sports[]" value="Vôlei"> Vôlei
+      <input type="checkbox" name="sports[]" value="Judô"> Judô
+    </div>
+    <div class="button">
+      <button type="submit">Enviar</button>
+    </div>
+  </form>
+  --->
+
+  <!---- Questão 02
+  <form action="dados.php" method="post">
+    <div class="content">
+      <input type="radio" name="age" value="18 anos"> 18 anos
+      <input type="radio" name="age" value="19 a 29 anos"> 19 a 29 anos
+      <input type="radio" name="age" value="30 a 50 anos"> 30 a 50 anos
+      <input type="radio" name="age" value="60 a 80 anos"> 60 a 80 anos
+    </div>
+    <div class="button">
+      <button type="submit">Enviar</button>
+    </div>
+  </form>
+  --->
+</body>
+</html>
